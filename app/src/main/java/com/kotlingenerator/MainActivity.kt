5e00ade960c4423af74850d9a99e387afa70816d
jinhan38 <jinhan38@naver.com>
2020-10-21 19:12:41 +0900

    commit

--- app/src/main/java/com/kotlingenerator/MainActivity.kt
+++ app/src/main/java/com/kotlingenerator/MainActivity.kt
@@ -7,7 +7,7 @@ import android.util.Log
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        private const val TAG = "MainActivity"
+        private const val TAG = "MainActivity" 
     }
 
 
